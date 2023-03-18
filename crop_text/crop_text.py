12f77487e611eca0e5ae711a81227a95284b40f7
Jwander <jasper870820@gmail.com>
2023-03-19 00:25:26 +0800

    feat: 裁切圖片新增指定底圖大小、設定膨脹迭帶次數
    1. 裁切圖片方法中新增指定底圖大小的功能，可以指定尺寸，會將裁切的圖片放到指定尺寸底圖中
    2. 膨脹迭代次數用於調節文字，若兩個文字太近想要分離的話，建議降低膨脹係數，若膨脹係數調整過低可能會造成文字的點被單獨分離

--- crop_text/crop_text.py
+++ crop_text/crop_text.py
@@ -1,11 +1,25 @@
 import cv2
 import numpy as np
 import os
+from PIL import Image
 
 
-def crop_text(img_path):
+def crop_text(img_path, limit_length=None, dilate_iter=10):
+    """
+    裁切圖片(白底)，並生成去背透明圖層，會根據框選出的輪廓做裁切
+    使用 limit_length 可以強制指定底圖尺寸大小，若不指定，則會生成裁切的輪廓尺寸*1.2的底圖
+    使用 dilate_iter 可以調整膨脹係數，若兩個文字間太靠近，則建議降低膨脹係數，須注意膨脹係數調整太小可能會造成文字獨立的點被分離
+    :param img_path: 圖片路徑
+    :param limit_length: 指定底圖大小
+    :param dilate_iter: 膨脹係數
+    :return:
+    """
     # 載入圖片
-    image = cv2.imread(img_path)
+    # image = cv2.imread(img_path)
+    image = Image.open(img_path)
+    image = image.convert("RGB")
+    image = np.uint8(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     # 轉換成灰階圖片
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -15,7 +29,7 @@ def crop_text(img_path):
 
     # 膨脹操作
     kernel = np.ones((3, 3), np.uint8)
-    dilated_image = cv2.dilate(threshold_image, kernel, iterations=10)
+    dilated_image = cv2.dilate(threshold_image, kernel, iterations=dilate_iter)
 
     # 尋找輪廓
     contours, hierarchy = cv2.findContours(dilated_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -41,7 +55,10 @@ def crop_text(img_path):
         extracted_image_rgba = cv2.merge(rgba, 4)
 
         # 建立透明底圖
-        transparent_image = np.zeros((160, 160, 4), dtype=np.uint8)
+        if limit_length:
+            transparent_image = np.zeros((limit_length, limit_length, 4), dtype=np.uint8)
+        else:
+            transparent_image = np.zeros((int(w*1.2), int(h*1.2), 4), dtype=np.uint8)
 
         # 計算圖片置中位置
         center_x = int(transparent_image.shape[1] / 2)
