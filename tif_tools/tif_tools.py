46402a5472e7f960b99fe729ad24b28e86a04655
Jwander0820 <66709644+Jwander0820@users.noreply.github.com>
2024-02-22 11:25:06 +0800

    feat: 整合合併多頁tif方法
    1. 整合合併多頁tif方法，將merge_img中合併多頁tif的方法整合進tif_tools中，方便使用
    2. 調整參數命名

--- tif_tools/tif_tools.py
+++ tif_tools/tif_tools.py
@@ -1,19 +1,20 @@
 import os
 
+import tifftools
 from PIL import Image
 
 
 class TifTools:
-    def save_multipage_tiff(self, image_paths, output_path, compression='tiff_lzw', convert=None):
+    def save_multipage_tiff(self, image_paths, output_path, compression='tiff_lzw', color_mode=None):
         """
         壓縮多個圖片成一個多頁的 tif 檔
         :param image_paths: 圖片檔案路徑列表
         :param output_path: 壓縮後的 tif 檔輸出路徑
         :param compression: 壓縮方法，預設為 'tiff_lzw'
-        :param convert: 位元深度轉換，根據PIL設定，例如 '1'、'L'、'RGB'、'RGBA'
+        :param color_mode: 位元深度轉換，根據PIL設定，例如 '1'、'L'、'RGB'、'RGBA'
         """
         if convert:
-            images = [Image.open(image_path).convert(convert) for image_path in image_paths]
+            images = [Image.open(image_path).convert(color_mode) for image_path in image_paths]
         else:
             images = [Image.open(image_path) for image_path in image_paths]
 
@@ -77,14 +78,39 @@ class TifTools:
             print(save_path)
         return img
 
+    def merge_img_to_one_tif(self, folder_path, save_file_path):
+        """
+        將資料夾下的所有圖片合成一張多幀TIF，合成多幀TIF是直接原圖合併成TIF檔，不受圖像尺寸限制
+        :param folder_path: 資料夾路徑
+        :param save_file_path: 儲存檔案的路徑
+        :return: 儲存檔案的路徑
+        """
+        files = os.listdir(folder_path)  # 讀取資料夾內所有資料
+        tif_files = []
+        for file in files:
+            if '.tif' in file or '.png' in file or '.jpg' in file:  # 檢測若file檔名包含.tif、.png、.jpg 加入要合併的清單
+                img_path = os.path.join(folder_path, file)
+                tif_files.append(img_path)
+        tif_files.sort()
+        # tifftools合成多幀tif檔案，效率佳又不吃記憶體；Image.save()也可以儲存多幀tif，但效率相對較差
+        tifftools.tiff_concat(tif_files, save_file_path, overwrite=True)
+        return save_file_path
+
 
 if __name__ == '__main__':
     # 將_path替換成想要拆頁的tif檔路徑執行即可
-    _path = r"D:\Database\圖像資料_保存用\金剛經六祖口訣-掃描檔\(掃描檔)金剛經六祖口訣-內文_對切.tif"
+    _path = r"./test_file.tif"
+    _page = 0
+
     tif_tools = TifTools()
 
     # 讀取或是儲存單一頁圖片
-    tif_tools.process_single_page(_path, 1, show=True, save=True)
+    tif_tools.process_single_page(_path, _page, save=True)
 
     # 分割多頁tif檔案成單一tif檔案
     # tif_tools.split_all_page(_path)
+
+    # 合併成多頁tif檔
+    # _folder_path = "../merge_img"
+    # _save_file_path = "./merge_img_to_one_TIF.tif"
+    # tif_tools.merge_img_to_one_tif(_folder_path, _save_file_path)
