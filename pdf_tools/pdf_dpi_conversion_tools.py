97452802d32a9afa3497172a1251d326c83685e4
Jwander0820 <66709644+Jwander0820@users.noreply.github.com>
2024-07-22 16:11:31 +0800

    feat: 更新pdf抽取工具
    1. 新增DPI預設config，透過config可以設定轉檔DPI，若無config則預設為192
    2. 新增--all參數，用於抽取整份PDF檔案，會另外儲存在以檔名為命名的資料夾內
    3. 新增--password參數，用於抽取加密後的PDF檔案

--- pdf_tools/pdf_dpi_conversion_tools.py
+++ pdf_tools/pdf_dpi_conversion_tools.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import os
 
 import fitz
 import numpy as np
@@ -8,9 +10,14 @@ from PIL import Image
 class PDFOperator:
     STANDARD_DPI = 72.0
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, password=None):
         self.file_path = file_path
         self.doc = fitz.open(file_path)
+        if self.doc.is_encrypted:
+            if password:
+                self.doc.authenticate(password)
+            else:
+                raise ValueError("PDF 文件被加密，請使用 --password 提供密碼。")
         self.total_pages = len(self.doc)
 
     def dimensions(self, page_number=0):
@@ -103,6 +110,40 @@ class PDFOperator:
         img.save(file_name)
 
 
+def load_default_dpi():
+    """
+    讀取設定檔，取得預設dpi，若無設定檔或設定檔參數異常，則欲設dpi為192
+    :return:
+    """
+    config_path = 'config.json'
+    if os.path.exists(config_path):
+        with open(config_path, 'r') as file:
+            config = json.load(file)
+        default_dpi = config.get('default_dpi', 192)
+    else:
+        default_dpi = 192  # 如果找不到配置文件，使用默認的 DPI
+    print(f'The default DPI setting is {default_dpi}')
+    return default_dpi
+
+
+def save_all_pages_as_images(processor, output_dir, dpi):
+    """
+    將所有頁面的 PDF 內容儲存為 PNG 檔案
+    :param processor: PDFOperator 實例
+    :param output_dir: 儲存圖片的目錄
+    :param dpi: 指定圖像的解析度
+    """
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    file_name_base = os.path.splitext(os.path.basename(processor.file_path))[0]
+    total_pages = processor.total_pages
+    for page in range(total_pages):
+        file_name = os.path.join(output_dir, f'{file_name_base}_page{str(page + 1).zfill(len(str(total_pages)))}.png')
+        processor.save_page_as_image(page, file_name, dpi=dpi)
+        print(f'Page {page + 1} has been saved as {file_name}')
+
+
 def main():
     """
     20240219；PDF指定DPI抽取圖片工具，使用PDFOperator做轉換，可簡易包裝成exe
@@ -110,21 +151,61 @@ def main():
     壓成EXE直接使用 pyinstaller -F ./folder/XXX.py 即可
     :return:
     """
+    default_dpi = load_default_dpi()
+
     parser = argparse.ArgumentParser(
         description='Convert a specific page of a PDF to an image with optional DPI setting.')
     parser.add_argument('pdf_path', type=str, help='Path to the PDF file.')
     parser.add_argument('--page', type=int, default=1, help='Page number to convert (default: 1).')
-    parser.add_argument('--dpi', type=int, default=96, help='DPI for the image (default: 96).')
+    parser.add_argument('--dpi', type=int, default=default_dpi, help='DPI for the image (default: 192).')
+    parser.add_argument('--password', type=str, default=None, help='Password for the encrypted PDF file.')
+    parser.add_argument('--all', action='store_true', help='Convert all pages of the PDF.')
     args = parser.parse_args()
 
-    # 注意：使用者界面中的頁碼從1開始，但內部實現從0開始，所以進行了-1操作
-    page = args.page - 1
-    dpi = args.dpi
+    try:
+        processor = PDFOperator(args.pdf_path, args.password)
+        dpi = args.dpi
 
-    processor = PDFOperator(args.pdf_path)
-    # 檔案名稱中加入指定的頁碼
-    file_name = args.pdf_path.replace('.pdf', f'_page{args.page}.png')
-    processor.save_page_as_image(page, file_name, dpi=dpi)
+        if args.all:
+            file_name_base = os.path.splitext(os.path.basename(processor.file_path))[0]
+            output_dir = os.path.join(os.path.dirname(args.pdf_path), file_name_base)
+            save_all_pages_as_images(processor, output_dir, dpi)
+            print(f'All pages have been saved in {output_dir}')
+
+        else:
+            # 注意：使用者界面中的頁碼從1開始，但內部實現從0開始，所以進行了-1操作
+            page_number = args.page
+            real_page_number = page_number - 1
+            file_name_base = args.pdf_path.replace('.pdf', f'')
+            file_name = f"{file_name_base}_page{str(page_number).zfill(len(str(processor.total_pages)))}.png"
+            processor.save_page_as_image(real_page_number, file_name, dpi=dpi)
+            print(f'Page {page_number} has been saved as {file_name}')
+
+    except Exception as e:
+        print(e)
+
+
+def main_test():
+    # 測試參數
+    pdf_path = r"file_path.pdf"
+    password = "password"  # 如果沒有密碼，可以設置為 None
+
+    processor = PDFOperator(pdf_path, password)
+    dpi = load_default_dpi()
+
+    # 測試單頁轉換
+    page_number = 1
+    real_page_number = page_number - 1
+    file_name_base = pdf_path.replace('.pdf', f'')
+    file_name = f"{file_name_base}_page{str(page_number).zfill(len(str(processor.total_pages)))}.png"
+    processor.save_page_as_image(real_page_number, file_name, dpi=dpi)
+    print(f'Page {page_number} has been saved as {file_name}')
+
+    # 測試所有頁面轉換
+    file_name_base = os.path.splitext(os.path.basename(processor.file_path))[0]
+    output_dir = os.path.join(os.path.dirname(pdf_path), file_name_base)
+    save_all_pages_as_images(processor, output_dir, dpi)
+    print(f'All pages have been saved in {output_dir}')
 
 
 if __name__ == '__main__':
